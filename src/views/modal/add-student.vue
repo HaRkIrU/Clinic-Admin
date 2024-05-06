178bea3601ac5ba9c993bca78748c816ba30e62e
jalimpia <teqcode@gmail.com>
2024-05-06 12:42:40 +0800

    Added List Students from API

---
+++ src/views/modal/add-student.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" @click="closeModal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            Your modal content goes here...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" @click="closeModal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      closeModal() {
+        const modal = document.getElementById('exampleModal');
+        if (modal) {
+            modal.classList.remove('show');
+            modal.style.display = 'none';
+        }
+      },
+    },
+  };
+  </script>
